6989614c1fbefa2d5ea67b6d5af04a71c17c3efb
D-Lopez21 <d.lopez@correo.unimet.edu.ve>
2022-02-12 12:53:22 -0400

    Hero y carrusel listo ahora si

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ botones.forEach(boton => {
       .querySelector("[data-slides]")
 
     const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    let newIndex = [...slides.children].indexOf(activeSlide)+ offset
     if (newIndex < 0) newIndex = slides.children.length - 1
     if (newIndex >= slides.children.length) newIndex = 0
 
